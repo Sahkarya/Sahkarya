bd73f2c40dbe46ab016b3f15ca81cfd69f347bbd
CodeWithNishant <nishantnagar54321@gmail.com>
2024-03-08 22:42:28 +0530

    Initiated the backend

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./utilities/db");
+
+const app = express();
+
+const PORT = 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
+});
+
+//mongodb password : B3HI5yJxlftDlrGP
