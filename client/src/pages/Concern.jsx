0f056022baf2577a4ca3d6e19d4443dc8ea5be8c
anujsolanki716 <anujsolanki716@gmail.com>
2024-03-12 14:53:26 +0530

    Added formated style

--- client/src/pages/Concern.jsx
+++ client/src/pages/Concern.jsx
@@ -22,72 +22,72 @@ const Concern = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} method="POST" style = {{display: "grid", justifyContent: "center", paddingLeft: "20px"}}>
-        <label htmlFor="name" style = {{fontSize: "17px"}}>Name:</label>
+      <form onSubmit={handleSubmit} method="POST" style={{ display: "grid", justifyContent: "center", paddingLeft: "20px" }}>
+        <label htmlFor="name" style={{ fontSize: "17px" }}>Name:</label>
         <br />
         <input
           type="text"
           id="name"
           name="name"
-          placeholder="Your name........"
+          placeholder="Your name"
           value={formData.name}
           onChange={handleChange}
           required
-        style = {{paddingLeft: "5px",boxSizing: "border-box", height: "40px", width: "430px",border: "1px solid #E6E6E6", borderRadius: "20px", color: "#333333", fontSize: "18px"}}/>
+          style={{ paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
         <br />
 
-        <label htmlFor="phone" style = {{fontSize: "17px"}}>Phone:</label>
+        <label htmlFor="phone" style={{ fontSize: "17px" }}>Phone:</label>
         <br />
         <input
           type="tel"
           id="phone"
           name="phone"
-          placeholder="It must be 10 digits........."
+          placeholder="Enter your phone"
           value={formData.phone}
           onChange={handleChange}
           required
-          style = {{paddingLeft: "5px",boxSizing: "border-box", height: "40px", width: "430px",border: "1px solid #E6E6E6", borderRadius: "20px", color: "#333333", fontSize: "18px"}}/>
+          style={{ paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
         <br />
 
-        <label htmlFor="message" style = {{fontSize: "17px"}}>Message:</label>
+        <label htmlFor="message" style={{ fontSize: "17px" }}>Message:</label>
         <br />
         <textarea
           id="message"
           name="message"
-          placeholder="e.g. I would like to ask you......."
+          placeholder="Write your concern here"
           value={formData.message}
           onChange={handleChange}
           required
-          style = {{paddingLeft: "5px",boxSizing: "border-box", height: "100px", width: "430px",border: "1px solid #E6E6E6", borderRadius: "20px", color: "#333333", fontSize: "18px"}}></textarea>
+          style={{ paddingLeft: "5px", boxSizing: "border-box", height: "100px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }}></textarea>
         <br />
 
-        <label htmlFor="address" style = {{fontSize: "17px"}}>Address:</label>
+        <label htmlFor="address" style={{ fontSize: "17px" }}>Address:</label>
         <br />
         <input
           type="text"
           id="address"
           name="address"
-          placeholder="Enter your street line......."
+          placeholder="Enter your address"
           value={formData.address}
           onChange={handleChange}
           required
-          style = {{paddingLeft: "5px",boxSizing: "border-box", height: "40px", width: "430px",border: "1px solid #E6E6E6", borderRadius: "20px", color: "#333333", fontSize: "18px"}}/>
+          style={{ paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
         <br />
 
-        <label htmlFor="department" style = {{fontSize: "17px"}}>Department:</label>
+        <label htmlFor="department" style={{ fontSize: "17px" }}>Department:</label>
         <br />
         <input
           type="text"
           id="department"
           name="department"
-          placeholder="Your department........"
+          placeholder="Department Responsible"
           value={formData.department}
           onChange={handleChange}
           required
-          style = {{paddingLeft: "5px",boxSizing: "border-box", height: "40px", width: "430px",border: "1px solid #E6E6E6", borderRadius: "20px", color: "#ffc107", fontSize: "18px"}}/>
+          style={{ paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#ffc107", fontSize: "18px" }} />
         <br />
 
-      <input type="submit" value="Submit" style = {{height: "40px", width:"245px", borderRadius: "20px",border:"none", backgroundColor:"#ffc107", marginBottom: "20px"}}/>
+        <input type="submit" value="Submit" style={{ height: "40px", width: "245px", borderRadius: "10px", border: "none", backgroundColor: "#ffc107", marginBottom: "20px" }} />
       </form>
     </>
   );
