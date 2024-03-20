dcfa5d4d5eb79dc40013128687c7a8716748585a
CodeWithNishant <nishantnagar54321@gmail.com>
2024-03-20 21:07:31 +0530

    fixed the phone number error

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -21,10 +21,34 @@ const RegistrationForm = () => {
   };
 
   return (
-    <div><h2 style={{ color: "rgb(0, 36, 71)", width: "100%", display: "grid", marginTop: "30px", alignItems: "center", justifyContent: "center", fontWeight: "600" }}>Registration Form</h2>
-      <form onSubmit={handleSubmit} style={{ display: "grid", justifyContent: "center", paddingLeft: "20px" }}>
-
-        <label htmlFor="name" style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}>Name:</label>
+    <div>
+      <h2
+        style={{
+          color: "rgb(0, 36, 71)",
+          width: "100%",
+          display: "grid",
+          marginTop: "30px",
+          alignItems: "center",
+          justifyContent: "center",
+          fontWeight: "600",
+        }}
+      >
+        Registration Form
+      </h2>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          paddingLeft: "20px",
+        }}
+      >
+        <label
+          htmlFor="name"
+          style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}
+        >
+          Name:
+        </label>
         <input
           type="text"
           id="name"
@@ -33,32 +57,77 @@ const RegistrationForm = () => {
           value={formData.name}
           onChange={handleChange}
           required
-          style={{ marginTop: "10px", paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
-        <label htmlFor="name" style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}>Identity:</label>
+          style={{
+            marginTop: "10px",
+            paddingLeft: "5px",
+            boxSizing: "border-box",
+            height: "40px",
+            width: "430px",
+            border: "1px solid #E6E6E6",
+            borderRadius: "10px",
+            color: "#333333",
+            fontSize: "18px",
+          }}
+        />
+        <label
+          htmlFor="name"
+          style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}
+        >
+          Identity:
+        </label>
         <input
           type="integer"
           id="identity"
           name="identity"
           placeholder="Enter your Id number."
-
           value={formData.identity}
           onChange={handleChange}
           required
-          style={{ marginTop: "10px", paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
-        <label htmlFor="phone" style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}>Phone:</label>
+          style={{
+            marginTop: "10px",
+            paddingLeft: "5px",
+            boxSizing: "border-box",
+            height: "40px",
+            width: "430px",
+            border: "1px solid #E6E6E6",
+            borderRadius: "10px",
+            color: "#333333",
+            fontSize: "18px",
+          }}
+        />
+        <label
+          htmlFor="phone"
+          style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}
+        >
+          Phone:
+        </label>
         <input
           type="tel"
           id="phone"
           name="phone"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
           placeholder="Your mobile number."
           value={formData.phone}
           onChange={handleChange}
           required
-          style={{ marginTop: "10px", paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
+          style={{
+            marginTop: "10px",
+            paddingLeft: "5px",
+            boxSizing: "border-box",
+            height: "40px",
+            width: "430px",
+            border: "1px solid #E6E6E6",
+            borderRadius: "10px",
+            color: "#333333",
+            fontSize: "18px",
+          }}
+        />
 
-
-        <label htmlFor="email" style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}>Email:</label>
+        <label
+          htmlFor="email"
+          style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}
+        >
+          Email:
+        </label>
         <input
           type="email"
           id="email"
@@ -67,10 +136,26 @@ const RegistrationForm = () => {
           value={formData.email}
           onChange={handleChange}
           required
-          style={{ content: "*", marginTop: "10px", paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
+          style={{
+            content: "*",
+            marginTop: "10px",
+            paddingLeft: "5px",
+            boxSizing: "border-box",
+            height: "40px",
+            width: "430px",
+            border: "1px solid #E6E6E6",
+            borderRadius: "10px",
+            color: "#333333",
+            fontSize: "18px",
+          }}
+        />
 
-
-        <label htmlFor="password" style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}>Password:</label>
+        <label
+          htmlFor="password"
+          style={{ marginTop: "30px", fontSize: "17px", fontWeight: "500" }}
+        >
+          Password:
+        </label>
         <input
           type="password"
           id="password"
@@ -79,9 +164,37 @@ const RegistrationForm = () => {
           value={formData.password}
           onChange={handleChange}
           required
-          style={{ marginTop: "10px", paddingLeft: "5px", boxSizing: "border-box", height: "40px", width: "430px", border: "1px solid #E6E6E6", borderRadius: "10px", color: "#333333", fontSize: "18px" }} />
+          style={{
+            marginTop: "10px",
+            paddingLeft: "5px",
+            boxSizing: "border-box",
+            height: "40px",
+            width: "430px",
+            border: "1px solid #E6E6E6",
+            borderRadius: "10px",
+            color: "#333333",
+            fontSize: "18px",
+          }}
+        />
 
-        <button type="submit" style={{ fontWeight: "500", height: "40px", width: "245px", marginTop: "30px", marginBottom: "30px", marginLeft: "20%", marginRight: "50%", borderRadius: "10px", border: "none", backgroundColor: "#ffc107", paddingTop: "5px" }}>Sign Up</button>
+        <button
+          type="submit"
+          style={{
+            fontWeight: "500",
+            height: "40px",
+            width: "245px",
+            marginTop: "30px",
+            marginBottom: "30px",
+            marginLeft: "20%",
+            marginRight: "50%",
+            borderRadius: "10px",
+            border: "none",
+            backgroundColor: "#ffc107",
+            paddingTop: "5px",
+          }}
+        >
+          Sign Up
+        </button>
       </form>
     </div>
   );
