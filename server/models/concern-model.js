ef792ccfa4a66da8563a49d2b8b9360fda2f3c9c
Nishant Nagar <nishantnagar54321@gmail.com>
2024-03-31 14:23:39 +0530

    all fields in the concern form are required

--- server/models/concern-model.js
+++ server/models/concern-model.js
@@ -3,10 +3,10 @@ const { Schema, model } = require("mongoose");
 const concernSchema = new Schema({
   email: { type: String, required: true },
   message: { type: String, required: true },
-  address: { type: String },
-  department: { type: String },
-  tag: { type: String },
-  image: { type: String },
+  address: { type: String, required: true },
+  department: { type: String, required: true },
+  tag: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
 const ConcernModel = new model("ConcernModel", concernSchema);
