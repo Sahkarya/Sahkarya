a4fcb939e514632e6eaea63e7b0def3b60801957
Nishant Nagar <nishantnagar54321@gmail.com>
2024-04-14 10:15:09 +0530

    completed user profile page with shitty css

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,31 +1,62 @@
 import { useAuth } from "../store/auth";
 import { useEffect, useState } from "react";
+import "./UserProfile.css";
 
 const UserProfile = () => {
   const { user } = useAuth();
   const [userData, setUserData] = useState(null);
+  const [userDataFetched, setUserDataFetched] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:80/api/data/user?email=${user.email}`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setUserData(data);
-        }
-      } catch (error) {
-        console.error("Error fetching user data", error);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:80/api/data/user?email=${user.email}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching user data", error);
+    }
+  };
 
+  if (user && !userDataFetched) {
     fetchData();
-  }, [user.email]);
+    setUserDataFetched(true);
+  }
 
-  console.log(userData);
   if (userData) {
     console.log(userData.msg);
+  }
+
+  if (!userData) {
+    return <p>Loading user data...</p>; // Or any loading indicator
+  } else {
+    return (
+      <>
+        <h1>Hello {user.name}🤗</h1>
+        <div className="userProfileContainer">
+          <div className="sidebar">
+            <h2>User Actions</h2>
+            <a href="">Edit Profile 📝</a>
+            <a href="">Change Password ✍️</a>
+            <a href="/concern">Raise New Concern 🫣</a>
+          </div>
+          <div className="content">
+            <h1>Your Concern History</h1>
+            {userData?.msg?.map((message) => (
+              <div key={message._id} className="concernCard">
+                <p>Status: {message.status.toString()}</p>
+                <p>Email: {message.email}</p>
+                <p>Message: {message.message}</p>
+                <p>Address: {message.address}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
   }
 };
 
